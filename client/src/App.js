df5958bec94ad73cc73e5147108c63ddea02e3b3
Alyssa Kwan <me@alyssackwan.name>
2017-02-25 12:18:58 -0800

    npm init

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Col, Row } from 'react-bootstrap';
+import { Provider } from 'react-redux';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello!
+      </div>
+    );
+  }
+}
+
+export default App;
