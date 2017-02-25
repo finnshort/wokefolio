921ffb10a3913ebf618a26e18f9de4ab6d1015c9
Alyssa Kwan <me@alyssackwan.name>
2017-02-25 13:37:08 -0800

    basic list of funds as an HTML table

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,18 @@ import { Provider } from 'react-redux';
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello!
+      <div class="container">
+      <div class="row">
+      <div class="col">
+      <div class="panel panel-default">
+        <div class="panel-heading">List of Funds</div>
+        <table class="table">
+          <tr><th>1</th><th></th><th></th><th></th><th>5</th></tr>
+          <tr><td></td><td></td><td></td><td></td><td></td></tr>
+        </table>
+      </div>
+      </div>
+      </div>
       </div>
     );
   }
